7d7e1d1257cd875051d989cc702e542c33a2f0cf
HalysonSA <halysonsa@gmail.com>
2023-12-08 22:44:54 -0300

    metatag and favicon

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,11 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <title>Robot master</title>
+        <meta name="description" content="Robot master speed runner" />
+      </Head>
+
       <body>
         <Main />
         <NextScript />
